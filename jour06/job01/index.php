701e5c154a36f0e3fc2a43688ecea0171d6d3f3e
franck-costanzo <franck.costanzo@laplateforme.io>
2022-03-18 15:24:43 +0100

    jour06

---
+++ jour06/job01/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job01</title>
+    <link rel="stylesheet" href="style1.css">
+    
+    <link rel="stylesheet" media="screen and (min-width: 1680px) and (max-width: 1920px)" href="style2.css" />
+    <link rel="stylesheet" media="screen and (min-width: 1280px) and (max-width: 1680px)" href="style3.css" />
+    <link rel="stylesheet" media="screen and (max-width: 1280px)" href="style4.css" />
+   
+</head>
+<body>
+    <h1>Job 01</h1>
+    
+</body>
+</html>
---
+++ jour06/job04/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job04</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+
+    <nav >
+        <div id="menuToggle">
+            <input type="checkbox" />
+
+            <span></span>
+            <span></span>
+            <span></span>
+
+            <ul id="menu">
+                <li><a href="">Accueil</a></li>
+                <li><a href="">Presentation</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </div>
+    </nav>
+    
+    </header>
+    <div>
+        <section class="section"></section>
+        <section class="section"></section>
+        <section class="section"></section>
+        <section class="section"></section>
+    </div>
+</body>
+</html>
